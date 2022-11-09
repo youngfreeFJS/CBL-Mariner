8b89900904adba566813c5663a0454d421387fe0
Sean Dougherty <inbox@seandougherty.dev>
2022-11-09 13:35:05 -0800

    Add dracut driver for overlay, dmverity,and vfat

--- toolkit/imageconfigs/additionalconfigs/configure-image.sh
+++ toolkit/imageconfigs/additionalconfigs/configure-image.sh
@@ -4,3 +4,5 @@ if [[ -f "/etc/systemd/network/99-dhcp-en.network" ]]; then
     echo Removing contents of 99-dhcp-en.network file
     truncate -s 0 /etc/systemd/network/99-dhcp-en.network
 fi
+
+mkinitrd
